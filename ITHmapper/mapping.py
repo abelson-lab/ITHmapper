fixed normalization to be consistent

ITHmapper/mapping.py
@@ -79,7 +79,7 @@ def score_reference_hotspot_modules(
         raise ValueError(f"Embedding key '{embedding_key}' not found in adata.obsm. Please compute the scvi or pca embedding first.")
     # Normalize, log1p, HVG selection
     adata_rep.X = adata_rep.layers["counts"].copy()
-    original_median_counts = np.loadtxt(f"reference_modules/original_median_counts__{cancer_type}.txt")
+    original_median_counts = np.loadtxt(f"reference_modules/median_counts/original_median_counts__{cancer_type}.txt")
     original_median_counts = int(np.median(original_median_counts))
     raw_total_counts = np.array(adata_rep.X.sum(axis=1)).flatten()
     adata_rep.X = (adata_rep.X.T / raw_total_counts).T * original_median_counts